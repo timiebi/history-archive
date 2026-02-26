f46ccf258d20585386fba6f8c946e69467701685
TIMIEBI <kosutimiebinicholas@gmail.com>
2026-02-26 11:10:14 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
     ],
+  },
+  turbopack: {
+    root: path.resolve(__dirname),
   },
 };
 
